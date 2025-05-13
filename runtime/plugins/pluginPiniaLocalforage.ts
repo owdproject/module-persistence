b4c78bfe0f633eb2bf02374c246500a2f53cd14e
dxlliv <hello@dxlliv.dev>
2025-05-13 02:33:09 +0200

    fix: Fix local storage import in pluginPiniaLocalforage.ts

--- runtime/plugins/pluginPiniaLocalforage.ts
+++ runtime/plugins/pluginPiniaLocalforage.ts
@@ -2,11 +2,7 @@ import { createPersistedStatePlugin } from 'pinia-plugin-persistedstate-2'
 import { defineNuxtPlugin } from "nuxt/app"
 import { usePinia } from "#imports"
 
-// this import will make the module work locally
-import * as localforage from 'localforage'
-
-// this import will make the module work when used as dependency
-// import 'localforage'
+import localforage from 'localforage'
 
 export default defineNuxtPlugin({
     name: 'owd-plugin-pinia-localforage',
