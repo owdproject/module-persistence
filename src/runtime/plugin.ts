78468f3e4e4cf0055972ac9ace43eaf767d58b02
dxlliv <dxlliv@proton.me>
2026-05-23 13:05:02 +0200

    fix: fix pinia usage

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,13 +1,15 @@
 import { createPersistedStatePlugin } from 'pinia-plugin-persistedstate-2'
 import { defineNuxtPlugin } from 'nuxt/app'
-import { usePinia } from 'pinia'
 import { get, set, del } from 'idb-keyval'
 
 export default defineNuxtPlugin({
   name: 'owd-plugin-persistence',
   parallel: true,
-  async setup() {
-    const pinia = usePinia()
+  async setup(nuxtApp) {
+    const pinia = nuxtApp.$pinia
+    if (!pinia) {
+      return
+    }
 
     pinia.use(
       createPersistedStatePlugin({
